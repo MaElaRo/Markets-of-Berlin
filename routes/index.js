f24408f4de4b58a1ba15e5ad425f04a5ac51e08d
Anna Dontcova <a.dontcova@gmail.com>
2019-07-09 14:40:01 +0200

    tried to get icons on map with Alican's code. Not working

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,36 @@ const express = require('express');
 const router  = express.Router();
 const mongoose = require("mongoose");
 const Market = require ("../models/Markets");
+const hbs = require("hbs");
+
+//Helper function to parse and display data on index
+hbs.registerHelper("JSON", data => JSON.stringify(data));
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+// router.get('/', (req, res, next) => {
+//   res.render('index');
+// });
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  Market.find({})
+    .then(markets => {
+      res.render("index", { markets });
+    })
+    .catch(err => {
+      console.log("Error while retrieving the markets: ", err);
+    });
 });
 
-router.get('/markets-of-berlin', (req, res, next) => {
-  Market.find({})
-    .then(marks => {
-      res.render("test", { markets: marks });
-    })
-    .catch(error => {
-      console.log(error)
-    })
-});
+// router.get('/markets-of-berlin', (req, res, next) => {
+//   Market.find({})
+//     .then(marks => {
+//       res.render("test", { markets: marks });
+//     })
+//     .catch(error => {
+//       console.log(error)
+//     })
+// });
 
 // router.get('/api/markets', (req, res, next) => {
 //   Place.find({}).then(markets=>{
